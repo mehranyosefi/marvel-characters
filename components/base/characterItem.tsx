a9229e3b548a9af2e9705e0a4c823049c8dbb3b9
mehranyosefi <u3efimehran@gmail.com>
2025-03-04 19:21:52 +0330

    add alt for image and reser nuxtimg component

--- components/base/characterItem.tsx
+++ components/base/characterItem.tsx
@@ -12,7 +12,7 @@ export default function characterItem(props: CharacterItemProps, { slots }: { sl
     return (
         <div class="border border-transparent hover:border-gray-500 hover:shadow-[0px_0px_0px_.5px] shadow-gray-500 rounded p-3 transition-colors duration-300 group">
             {slots.default ? slots.default() : <>
-                <nuxt-img src={SrcCalculate} class="w-full h-96 rounded transition duration-500 group-hover:scale-104"></nuxt-img>
+                <nuxt-img src={SrcCalculate} alt={`image of ${name}`} class="w-full h-96 rounded transition duration-500 group-hover:scale-104"></nuxt-img>
                 <div class="mt-5">
                     <span >{name}</span>
                 </div></>}
